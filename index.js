6cfb86b6c8a105665c523e27713d0b166dfa2ebf
Michael Matuzak <mmatuzak@gmail.com>
2013-10-29 21:20:37 -0700

    fixing a bug where if the game is larger than the chunk that is being streamed all the parsing still works

--- index.js
+++ index.js
@@ -12,6 +12,14 @@ function bytesToSize(bytes) {
 module.exports = function (file, cb) {
     var rom = false;
     var exists = fs.existsSync(file);
+
+    var prgSize = 0;
+    var chrSize = 0;
+
+    var chrBlockSize = 8192;
+    var prgBlockSize = 16384;
+
+    var nesHeader;
     var prgLen;
     var chrLen;
     var byte6;
@@ -20,36 +28,50 @@ module.exports = function (file, cb) {
      
 
     var write = function (chunk) {
-        // first 3 bytes should be NES
-        var nesHeader = chunk.slice(0,3);
+        if (!nesHeader) {
+            // first 3 bytes should be NES
+            nesHeader = chunk.slice(0,3);
 
-        var chrBlockSize = 8192;
-        var prgBlockSize = 16384;
+            if (nesHeader.toString() !== 'NES') console.log('WARN: might not be a NES file.');
 
-        var prgSize = 0;
-        var chrSize = 0;
+            // inesprg: 1x 16KB PRG code
+            prgLen = parseInt(chunk.slice(4,5).toString('hex'), 16);
 
-        if (nesHeader.toString() !== 'NES') console.log('WARN: might not be a NES file.');
-        
-        // inesprg: 1x 16KB PRG code
-        prgLen = parseInt(chunk.slice(4,5).toString('hex'), 16);
+            // ineschr: 1x  8KB CHR data
+            chrLen = parseInt(chunk.slice(5,6).toString('hex'), 16);
+            prgSize = prgBlockSize * prgLen;
+            chrSize = chrBlockSize * chrLen;
+            /*  bit 0 1 for vertical mirroring, 0 for horizontal mirroring.
+                bit 1 1 for battery-backed RAM at $6000-$7FFF.
+                bit 2 1 for a 512-byte trainer at $7000-$71FF.
+                bit 3 1 for a four-screen VRAM layout.
+                bit 4-7  Four lower bits of ROM Mapper Type.
+            */
+            byte6 = chunk.slice(6,7);
+        }
 
-        // ineschr: 1x  8KB CHR data
-        chrLen = parseInt(chunk.slice(5,6).toString('hex'), 16);
+        if (!prg) {
+            if (chunk.length <= prgSize) {
+                prg = chunk.slice(7, prgSize - chunk.length);
+                prgSize -= chunk.length;
+            } else {
+                prg = chunk.slice(7, prgSize);
+                chr = chunk.slice(prgSize, prgSize + chrSize);
+                prgSize = 0;
+            }
+        } else if (prgSize > 0) {
+            if (chunk.length >= prgSize) {
+                prg = Buffer.concat([prg, chunk.slice(0, prgSize - chunk.length)]);
+                prgSize -= chunk.length;
+            } else {
+                prg = Buffer.concat([prg, chunk.slice(0, prgSize)]);
+                prgSize = 0;
+            }
+        }
 
-        prgSize = prgBlockSize * prgLen;
-        chrSize = chrBlockSize * chrLen;
-
-        /*  bit 0 1 for vertical mirroring, 0 for horizontal mirroring.
-            bit 1 1 for battery-backed RAM at $6000-$7FFF.
-            bit 2 1 for a 512-byte trainer at $7000-$71FF.
-            bit 3 1 for a four-screen VRAM layout.
-            bit 4-7  Four lower bits of ROM Mapper Type.
-         */
-        byte6 = chunk.slice(6,7).toString();
-
-        prg = chunk.slice(7, prgSize);
-        chr = chunk.slice(prgSize, prgSize + chrSize);
+        if (!chr && chrSize !== 0 && prgSize === 0) {
+            chr = chunk.slice(0, chrSize);
+        }
     };
 
     var end = function () {
